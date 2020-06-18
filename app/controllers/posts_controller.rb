99ab486ba04e083cc31719daacdc0a67f139d217
tessy-man1120 <k.tessyma1120@gmail.com>
2020-06-18 23:29:54 +0900

    stay_create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(10)
+    # @user = User.find(params[:id])
   end
 
   def new
