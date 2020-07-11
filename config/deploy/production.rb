05acb9cbade861a5df8d39244a3fb6c094936422
tessy-man1120 <k.tessyma1120@gmail.com>
2020-07-11 11:45:50 +0900

    re_server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-# server '54.150.240.166', user: 'ec2-user', roles: %w{app db web}
+server '54.150.240.166', user: 'ec2-user', roles: %w{app db web}
 
 server 'mygrowdiary2020.site', user: 'ec2-user', roles: %w{app db web}
