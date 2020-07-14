00db9d56d31646ad296bbe87d9c25b9247905845
tessy-man1120 <k.tessyma1120@gmail.com>
2020-07-14 21:20:02 +0900

    nickname_edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :profile, :image])
   end
 end
