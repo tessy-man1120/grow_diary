4faa6541b076f8ad7089e5716b4aeae02cb6f122
tessy-man1120 <k.tessyma1120@gmail.com>
2020-07-15 00:34:43 +0900

    comment_down_scroll

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,12 +1,28 @@
+// 上までのスクロール
 $(document).ready(function(){
-  $(window).scroll(function() {
-      if($(this).scrollTop() > 100) { 
-          $(".back-to-top").fadeIn(); 
-      } else {
-          $(".back-to-top").fadeOut();
-      }
-  });
-  $(".back-to-top").click(function() {
-      $("body,html").animate({scrollTop:0},900); 
-  });
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 100) { 
+            $(".back-to-top").fadeIn(); 
+        } else {
+        $(".back-to-top").fadeOut();
+        }
+    });
+    $(".back-to-top").click(function() {
+        $("body,html").animate({scrollTop:0},900); 
+    });
 });
+
+// コメント欄へのスクロール
+$(function(){
+    function ScrollButton(button, place){
+    var position = $(place).offset().top;  
+    $(button).click(function(){
+        $("html,body").animate({
+            scrollTop : position
+        }, {
+            queue : false
+        });
+    });
+    }
+    ScrollButton(".postInfo__commentIcon", ".comments__title");
+    });
