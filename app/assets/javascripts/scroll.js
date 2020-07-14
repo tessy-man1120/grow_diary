0730bd1c788acc764d39838f490ccea0efc2c4e0
tessy-man1120 <k.tessyma1120@gmail.com>
2020-07-14 19:37:02 +0900

    scrollUp

---
+++ app/assets/javascripts/scroll.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $(window).scroll(function() {
+      if($(this).scrollTop() > 100) { 
+          $(".back-to-top").fadeIn(); 
+      } else {
+          $(".back-to-top").fadeOut();
+      }
+  });
+  $(".back-to-top").click(function() {
+      $("body,html").animate({scrollTop:0},900); 
+  });
+});
