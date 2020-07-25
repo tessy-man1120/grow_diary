3e07f09bdd92c8bb140e8bd0e19516d7e02c46e5
tessy-man1120 <k.tessyma1120@gmail.com>
2020-07-25 17:44:01 +0900

    before_action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  # before_action :move_to_index, except: [:index, :show, :search, :rank]
+  before_action :move_to_index, except: [:index, :show, :search, :rank]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -7,26 +7,23 @@ describe PostsController do
       expect(response).to render_template :new
     end
   end
-  # describe 'GET #edit' do
-  #   it "@postに正しい値が入っていること" do
-  #     post = create(:post)
-  #     get :post, params: { id: post}
-  #     expect(assigns(:post)).to eq post
-  #   end
-
-  #   it "edit.html.hamlに遷移すること" do
-  #     post = create(:post)
-  #     get :post, params: { id: post }
-  #     expect(response).to render_template :edit
-  #   end
-  # end
-  describe 'GET #index' do
+  describe 'GET #edit' do
     it "@postに正しい値が入っていること" do
-      posts = create_list(:post, 3)
-      get :index
-      expect(assigns(:posts)).to match(posts)
+      post = create(:post)
+      get :post, params: { id: post}
+      expect(assigns(:post)).to eq post
     end
+
+    it "edit.html.hamlに遷移すること" do
+      post = create(:post)
+      get :post, params: { id: post }
+      expect(response).to render_template :edit
+    end
+  end
+  describe 'GET #index' do
     it "index.html.hamlに遷移すること" do
+      get :index
+      expect(response).to render_template :index
     end
   end
 end
